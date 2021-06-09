4ed5d05160623dc9ddd669b7c627682960b9431b
Roberto Vega <robertovegab777@gmail.com>
2021-06-09 16:36:41 -0500

    prueba calculate

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,9 @@ import {MatSliderChange} from "@angular/material/slider";
 import {MatSelectChange} from '@angular/material/select';
 import {RatesApiService} from 'src/app/services/rates-api.service';
 import {MatTableDataSource} from '@angular/material/table';
+import {Rates} from 'src/app/models/rates';
+//import {mainCalc} from './calculo';
+import {Calculate} from './calculate';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +18,8 @@ export class HomeComponent implements OnInit {
 
   calculateForm: FormGroup;
   mortgageData: MortgageCredit;
+  calculate: Calculate;
+  rateCalculate: Rates;
   displayedColumns: string[] = ['id', 'bank', 'term', 'value', 'minRate', 'maxRate', 'favorite'];
   dataSource = new MatTableDataSource();
   sol: Boolean = true;
@@ -22,6 +27,8 @@ export class HomeComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder, private ratesApi: RatesApiService) {
     this.mortgageData = {} as MortgageCredit;
+    this.calculate = {} as Calculate;
+    this.rateCalculate = {} as Rates;
     this.mortgageData.currency = "soles";
     this.calculateForm = this.formBuilder.group({
       property_value: [null, null],
@@ -45,6 +52,7 @@ export class HomeComponent implements OnInit {
       this.dataSource.data = response;
       this.isFill = true;
     })
+    this.onCalculate();
   }
   onInputChange(event:MatSliderChange){
     console.log(event.value)
@@ -53,5 +61,11 @@ export class HomeComponent implements OnInit {
     if(event.value != "Soles"){
       this.sol = false;
     }else this.sol = true;
+  }
+
+  onCalculate() {
+    console.log('onCalculate');
+    //console.log(this.dataSource.data[0]);
+    this.calculate.mainCalc(0.07, 390000, 240, 10000, 0.10)
   }
 }
