301e7e8443e91c744dba955c76d5479c3902b380
RaulC2803 <raul_casanova_28@hotmail.com>
2021-06-25 16:59:47 -0500

    feat: login better and home component with methods done

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,9 +3,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {MortgageCredit} from 'src/app/models/mortgage-credit';
 import {MatSelectChange} from '@angular/material/select';
 import {RatesApiService} from 'src/app/services/rates-api.service';
-import {MatTableDataSource} from '@angular/material/table';
 import {Rates} from 'src/app/models/rates';
 import {Calculate} from './calculate';
+import {UserApiService} from 'src/app/services/user-api.service';
+import {User} from 'src/app/models/user';
+import {TokenStorageService} from 'src/app/services/token-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -15,19 +17,20 @@ import {Calculate} from './calculate';
 export class HomeComponent implements OnInit {
 
   calculateForm: FormGroup;
-  mortgageData: MortgageCredit;
   calculate: Calculate;
-  rateCalculate: Rates[];
   dataSourceList : MortgageCredit[] = [];
+  assetstList: string[] = [];
+  classList: string[] = [];
+  scotia_img: string = "../../../assets/images/scotia.png";
+  interb_img: string = "../../../assets/images/interb.jpg";
 
   sol: Boolean = true;
   isFill: Boolean = false;
   onlyNumberPattern: string = "^[0-9]*$";
 
-  constructor(private formBuilder: FormBuilder, private ratesApi: RatesApiService) {
-    this.mortgageData = {} as MortgageCredit;
+  constructor(private formBuilder: FormBuilder, private ratesApi: RatesApiService,
+             private userApi: UserApiService, private tokenStorage: TokenStorageService) {
     this.calculate = new Calculate();
-    this.rateCalculate = [];
     this.calculateForm = this.formBuilder.group({
       property_value: [null, [Validators.required, Validators.min(1000), Validators.pattern(this.onlyNumberPattern)]],
       income: [null, [Validators.required, Validators.pattern(this.onlyNumberPattern)]],
@@ -42,19 +45,71 @@ export class HomeComponent implements OnInit {
   }
 
   onSubmit(): void {
+    this.dataSourceList = [];
+    this.assetstList = [];
     this.getRates();
     this.isFill = true;
   }
 
+  onCalculate(income: number, initial_fee:number,method: string,
+              property_value:number, term:number, rate: Rates, currency:string): void{
+    if(rate.bank_id == 1){
+      this.assetstList.push(this.scotia_img);
+      this.assetstList.push(this.scotia_img);
+      this.classList.push("scotiabank");
+      this.classList.push("scotiabank");
+    }else{
+      this.assetstList.push(this.interb_img);
+      this.assetstList.push(this.interb_img);
+      this.classList.push("interbank");
+      this.classList.push("interbank");
+    }
+    switch(method) {
+      case "frances": {
+        // Min Rate
+        this.calculate.french_method(property_value, initial_fee/100, term, rate.min_rate/100);
+        this.nextCalculate(income, initial_fee, property_value, term, currency);
+        // Max Rate
+        this.calculate.french_method(property_value, initial_fee/100, term, rate.max_rate/100);
+        this.nextCalculate(income, initial_fee, property_value, term, currency);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  nextCalculate(income: number, initial_fee:number,
+              property_value:number, term:number, currency:string): void{
+    const mortgage = {} as MortgageCredit;
+    mortgage.term = term;
+    mortgage.initial_fee =initial_fee;
+    mortgage.currency = currency;
+    mortgage.property_value = property_value;
+    mortgage.monthly_fee = -(this.calculate.cuotas[this.calculate.cuotas.length-1]);
+    mortgage.incomes = income;
+    mortgage.tcea = 11.0;
+    this.dataSourceList.push(mortgage);
+  }
+
   getRates(): void {
-    this.ratesApi.getRateByValueAndFeeValue(this.mortgageData.term, this.mortgageData.property_value, this.mortgageData.currency).subscribe((res: Rates[]) => {
+    const currency = this.calculateForm.value.currency;
+    const income = parseFloat(this.calculateForm.value.income);
+    const initial_fee = this.calculateForm.value.initial_fee;
+    const method = this.calculateForm.value.method;
+    const property_value = parseFloat(this.calculateForm.value.property_value);
+    const term = this.calculateForm.value.term;
+    this.ratesApi.getRateByValueAndFeeValue(term, property_value, currency).subscribe((res: Rates[]) => {
       res.forEach(rate => {
-        console.log(rate)
-        this.calculate.french_method(
-          this.mortgageData.property_value, this.mortgageData.initial_fee/100, this.mortgageData.term, rate.min_rate/100);
-        console.log(this.calculate.cuotas);
-        this.nextToCalculate();
+        this.onCalculate(income, initial_fee, method, property_value, term, rate, currency);
       })
+    })
+  }
+
+  handleSaveButton(index: number): void{
+    this.userApi.saveMortgageCreditByUserId(this.dataSourceList[index], this.tokenStorage.getUser().id).subscribe((res: User) => {
+      console.log(res);
     })
   }
 
@@ -65,12 +120,5 @@ export class HomeComponent implements OnInit {
   }
   formatSliderInitialFee(value: number){
     return value + '%';
-  }
-
-  nextToCalculate() {
-    this.mortgageData.monthly_fee = -(this.calculate.cuotas[this.calculate.cuotas.length-1]);
-    //this.mortgageData.tcea = parseFloat((this.calculate.tcea * 100).toFixed(4));
-    this.mortgageData.tcea = 11.0000;
-    this.dataSourceList.push(this.mortgageData);
   }
 }
