85f0579d51b307f559caf992c0d54d127221d888
RaulC2803 <raul_casanova_28@hotmail.com>
2021-07-01 01:56:35 -0500

    feat: help panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import {TokenStorageService} from './services/token-storage.service';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenStorageService } from './services/token-storage.service';
+import { MatDialog } from '@angular/material/dialog'
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,11 @@ export class AppComponent implements OnInit {
   title = 'credi-compara';
   isLoggedIn = false;
   username: string = '';
+  innerWidth: any;
+  lessThan500: Boolean = false;
   constructor(private tokenStorageService: TokenStorageService,
-             private router: Router){}
+             private router: Router,
+             public dialog: MatDialog){}
 
   ngOnInit(): void{
     this.isLoggedIn = !!this.tokenStorageService.getToken();
@@ -22,9 +26,33 @@ export class AppComponent implements OnInit {
     }else{
       this.router.navigate(['/login']);
     }
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth <= 500){
+      this.lessThan500 = true;
+    }else{
+      this.lessThan500 = false;
+    }
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(){
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth <= 500){
+      this.lessThan500 = true;
+    }else{
+      this.lessThan500 = false;
+    }
   }
   logout(): void{
     this.tokenStorageService.signOut();
     window.location.reload();
   }
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+  }
 }
+
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: 'dialog-content-example-dialog.html',
+})
+export class DialogContentExampleDialog {}
